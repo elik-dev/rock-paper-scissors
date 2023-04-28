80637064bc124d6dbf48f9bd8dfecbc560c21af4
Elik <130469789+elik-dev@users.noreply.github.com>
2023-04-29 00:12:52 +0600

    Add various functions and update README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rock-paper-scissors
 In this project I'm going to test skills obtained from JavaScript Basics section of The Odin Project
 This will be a basic rock paper scissors game
-With input/output through console
+The player input will be through prompt window and the output through alert
--- js/script.js
+++ js/script.js
@@ -1,12 +1,63 @@
 function getComputerChoice() {
-    // generate random number from 0 to 2 and store in i
-    let i = Math.floor(Math.random() * 3);
-    // return choice based on i value
-    if (i === 0) {
+    // generate random number from 0 to 2 and store it in random0to2
+    let random0to2 = Math.floor(Math.random() * 3);
+    // return choice depending on random0to2 value
+    if (random0to2 === 0) {
         return "rock";
-    } else if (i === 1) {
+    } else if (random0to2 === 1) {
         return "paper";
     } else {
         return "scissors";
+    }
+}
+// ask player to input his choice
+// ask again if it's not rock, paper or scissors
+function getPlayerChoice() {
+    let playerInput = prompt("Rock, Paper or Scissors?").toLowerCase();
+    switch (playerInput) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            break;
+        default:
+            playerInput = getPlayerChoice();
+    }
+    return playerInput;
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function playRound() {
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    let winMsg = `You won. ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`
+    let loseMsg = `You lost. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`
+    let drawMsg = `Draw. ${capitalize(playerSelection)} vs ${capitalize(computerSelection)}.`;
+    if (playerSelection === computerSelection) {
+        alert(drawMsg);
+        return drawMsg;
+    }
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert(winMsg);
+        return winMsg;
+    } else if (playerSelection === 'rock') {
+        alert(loseMsg);
+        return loseMsg;
+    }
+    if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert(winMsg);
+        return winMsg;
+    } else if (playerSelection === 'paper') {
+        alert(loseMsg);
+        return loseMsg;
+    }
+    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert(winMsg);
+        return winMsg;
+    } else if (playerSelection === 'scissors') {
+        alert(loseMsg);
+        return loseMsg;
     }
 }
