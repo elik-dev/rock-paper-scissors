46db4076473d971c8a1bb0e47ecb074154613764
Elik <130469789+elik-dev@users.noreply.github.com>
2023-04-29 18:39:55 +0600

    Add more helper functions and rewrite existing ones

--- js/script.js
+++ js/script.js
@@ -1,63 +1,84 @@
+// generate computer choice
 function getComputerChoice() {
-    // generate random number from 0 to 2 and store it in random0to2
+    // generate random number from 0 to 2 and store it in random0to2 variable
     let random0to2 = Math.floor(Math.random() * 3);
-    // return choice depending on random0to2 value
+    // return computer choice based on random0to2 value
     if (random0to2 === 0) {
-        return "rock";
+        return 'rock';
     } else if (random0to2 === 1) {
-        return "paper";
+        return 'paper';
     } else {
-        return "scissors";
+        return 'scissors';
     }
 }
-// ask player to input his choice
-// ask again if it's not rock, paper or scissors
+// input player choice
 function getPlayerChoice() {
-    let playerInput = prompt("Rock, Paper or Scissors?").toLowerCase();
-    switch (playerInput) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            break;
-        default:
-            playerInput = getPlayerChoice();
+    let validChoice = false;
+    let playerInput;
+    while (validChoice === false) {
+        playerInput = prompt("Rock, Paper or Scissors?").toLowerCase();
+        switch (playerInput) {
+            case 'rock':
+            case 'paper':
+            case 'scissors':
+                validChoice = true;
+                break;
+            default:
+                alert('Please enter a valid option: Rock, Paper or Scissors');
+                validChoice = false;
+                break;
+        }
     }
     return playerInput;
 }
-
+// capitalize first letter of a string
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
-
+// play one round of game
 function playRound() {
+    // get player and computer choices and store them in following variables
     let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
-    let winMsg = `You won. ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`
-    let loseMsg = `You lost. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`
-    let drawMsg = `Draw. ${capitalize(playerSelection)} vs ${capitalize(computerSelection)}.`;
-    if (playerSelection === computerSelection) {
-        alert(drawMsg);
-        return;
+    // store game result messages in following variables
+    let winMessage = `You won. ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}.`;
+    let loseMessage = `You lost. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}.`;
+    let drawMessage = `Tie. ${capitalize(playerSelection)} vs ${capitalize(computerSelection)}.`;
+    // check round result and return corresponding message
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        console.log(winMessage);
+        return 'win';
+    } else if (playerSelection === computerSelection) {
+        console.log(drawMessage);
+        return 'tie';
+    } else {
+        console.log(loseMessage);
+        return 'lose';
     }
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        alert(winMsg);
-        return;
-    } else if (playerSelection === 'rock') {
-        alert(loseMsg);
-        return;
+}
+// play 5 rounds and return winner of a game
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        if (result === 'win') {
+            playerScore += 1;
+        } else if (result === 'lose') {
+            computerScore += 1;
+        }
     }
-    if (playerSelection === 'paper' && computerSelection === 'rock') {
-        alert(winMsg);
+    let finalScore = `Final score: ${playerScore} - ${computerScore}`;
+    if (playerScore > computerScore) {
+        console.log(`Victory. ${finalScore}`);
         return;
-    } else if (playerSelection === 'paper') {
-        alert(loseMsg);
+    } else if (playerScore === computerScore) {
+        console.log(`Draw. ${finalScore}`);
         return;
-    }
-    if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        alert(winMsg);
-        return;
-    } else if (playerSelection === 'scissors') {
-        alert(loseMsg);
+    } else {
+        console.log(`Defeat. ${finalScore}`);
         return;
     }
 }
