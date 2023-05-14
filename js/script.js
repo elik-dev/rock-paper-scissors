5076d9c88fa36c9d7f7bc1bc1cb4fc776cac85fa
Elik <130469789+elik-dev@users.noreply.github.com>
2023-05-14 16:52:01 +0600

    Add player icon interactivity and optimize code

--- js/script.js
+++ js/script.js
@@ -1,49 +1,63 @@
 function getComputerChoice() {
-    const handSigns = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * handSigns.length);
-    return handSigns[randomIndex];
+    const selectionList = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * selectionList.length);
+    return selectionList[randomIndex];
 }
-function restartGame() {
-    const gameWindow = document.querySelector(".game-window");
-    const result = document.querySelector(".round-result");
-    const resultScreen = document.querySelector(".result-screen");
+function restartGame(evt) {
+    const gameEnd = document.querySelector(".game-end");
     playerScore.textContent = 0;
     computerScore.textContent = 0;
-    result.textContent = "Win 5 rounds";
-    if (resultScreen) gameWindow.removeChild(resultScreen);
-    return;
+    roundResult.textContent = "Win 5 rounds";
+    playerImage.setAttribute("src", "img/placeholder-player.png");
+    computerImage.setAttribute("src", "img/placeholder-computer.png");
+    if (gameEnd) gameWindow.removeChild(gameEnd);
 }
 function finishGame(result) {
-    const gameWindow = document.querySelector(".game-window");
-    const resultScreen = document.createElement("div");
+    const currentScore = document.querySelector(".current-score");
 
-    resultScreen.setAttribute("class", "result-screen");
-    gameWindow.appendChild(resultScreen);
+    const gameEnd = document.createElement("div");
+    gameEnd.classList.add("game-end");
+    gameWindow.appendChild(gameEnd);
 
-    const gameResult = document.createElement("p")
-    gameResult.setAttribute("class", "game-result");
+    const gameResult = document.createElement("p");
+    gameResult.classList.add("game-result");
     gameResult.textContent = result;
-    resultScreen.appendChild(gameResult);
+    gameEnd.appendChild(gameResult);
+
+    const finalScore = document.createElement("p");
+    finalScore.classList.add("final-score");
+    finalScore.textContent = `Final score: ${currentScore.textContent}`;
+    gameEnd.appendChild(finalScore);
 
     const restartButton = document.createElement("button");
+    restartButton.classList.add("restart-button");
     restartButton.addEventListener('click', restartGame);
     restartButton.textContent = "Play again";
-    resultScreen.appendChild(restartButton);
+    gameEnd.appendChild(restartButton);
+}
+function addClickEffects(evt) {
+    evt.preventDefault();
+    evt.currentTarget.classList.add("selection-active");
+}
+function removeTransition(evt) {
+    evt.preventDefault();
+    evt.currentTarget.classList.remove("highlight-blue", "selection-active");
 }
 function playRound(evt) {
     const playerSelection = evt.target.getAttribute("alt");
-    let roundResult = document.querySelector(".round-result");
     const computerSelection = getComputerChoice();
+    playerImage.setAttribute("src", evt.target.getAttribute("src"));
+    computerImage.setAttribute("src", document.querySelector(`img[alt="${computerSelection}"]`).getAttribute("src"));
+    roundResult.classList.add("highlight-blue");
+    addClickEffects(evt);
     if (playerSelection === "Restart") {
-        restartGame();
-        return;
-    };
+        return restartGame(evt);
+    }
     if ((playerSelection === "Rock" && computerSelection === "Scissors")
         || (playerSelection === "Paper" && computerSelection === "Rock")
         || (playerSelection === "Scissors" && computerSelection === "Paper")) {
         roundResult.textContent = "Round won";
         playerScore.textContent++;
-
     } else if (playerSelection === computerSelection) {
         roundResult.textContent = "Tie :|"
     } else {
@@ -52,15 +66,22 @@ function playRound(evt) {
     }
     if (playerScore.textContent == 5) {
         finishGame("Victory!");
-        return;
     } else if (computerScore.textContent == 5) {
         finishGame("Defeat :(");
-        return;
     }
 }
-
 let playerScore = document.querySelector('.player-score');
 let computerScore = document.querySelector('.computer-score');
 
+const gameWindow = document.querySelector(".game");
+const playerImage = document.querySelector(".player-choice img");
+const computerImage = document.querySelector(".computer-choice img");
+const roundResult = document.querySelector(".round-result");
+
 const selections = document.querySelectorAll("div.selection");
-selections.forEach(selection => selection.addEventListener("click", playRound));
+selections.forEach(selection => {
+    selection.addEventListener("mousedown", playRound);
+    selection.addEventListener("touchstart", playRound);
+    selection.addEventListener("transitionend", removeTransition);
+});
+roundResult.addEventListener("transitionend", removeTransition);
